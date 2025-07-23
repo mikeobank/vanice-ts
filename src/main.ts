626ae19bfb2a643715fb4d43b6d6ea03a0a1bccc
Mike O'Bank <mike.obank@pm.me>
2025-07-23 14:40:10 +0200

    Remove iffe, add guesses / minute logging

--- src/main.ts
+++ src/main.ts
@@ -4,53 +4,59 @@ import encodeToPrime from "./lib/encodeToPrime.ts"
 import toVanityKey, { type PrimeKey } from "./lib/toVanityKey.ts"
 import isVanity from "./lib/isVanity.ts"
 
-(async () => {
+// CLI arg
+if (Deno.args[0] === undefined) {
+  console.error("No vanity name provided")
+  Deno.exit()
+}
+const vanity = Deno.args[0].trim()
+if (vanity === "") {
+  console.error("Empty vanity name provided")
+  Deno.exit()
+}
+if (isVanity(vanity) === false) {
+  console.error(`Invalid characters in vanity name provided: ${ vanity }`)
+  Deno.exit()
+}
 
-  // CLI arg
-  if (Deno.args[0] === undefined) {
-    console.error("No vanity name provided")
-    Deno.exit()
-  }
-  const vanity = Deno.args[0].trim()
-  if (vanity === "") {
-    console.error("Empty vanity name provided")
-    Deno.exit()
-  }
-  if (isVanity(vanity) === false) {
-    console.error(`Invalid characters in vanity name provided: ${ vanity }`)
-    Deno.exit()
-  }
+// Search string
+const search = vanityToPrime(vanity)
+const searchLength = search.length
+console.log(`Searching for vanity name: ${ vanity } (${ search })`)
 
-  // Search string
-  const search = vanityToPrime(vanity)
-  const searchLength = search.length
-  console.log(`Searching for vanity name: ${ vanity } (${ search })`)
+// Looping variables
+let match = false
+let privateKey: PrivateKey
+let publicKey: PublicKey
+let primeKey: PrimeKey
+let i = 0
 
-  // Looping variables
-  let match = false
-  let privateKey: PrivateKey
-  let publicKey: PublicKey
-  let primeKey: PrimeKey
-  let i = 0
+// Logging variables
+let lastI = 0
+let lastLog = Date.now()
 
-  // TODO: Multi-threading, Worker
-  while (match === false) {
-    [privateKey, publicKey] = generateKeyPair()
-    // TODO: Only encode first searchLength characters / bytes
-    primeKey = encodeToPrime(publicKey)
-    const v = primeKey.substring(0, searchLength)
-    if (v === search) {
-      match = true
-    } else {
-      i++
-    }
-    if (i % 10000 === 0) {
-      console.log(i)
-    }
+// TODO: Multi-threading, Worker
+while (match === false) {
+  [privateKey, publicKey] = generateKeyPair()
+  // TODO: Only encode first searchLength characters / bytes
+  primeKey = encodeToPrime(publicKey)
+  const v = primeKey.substring(0, searchLength)
+  if (v === search) {
+    match = true
+  } else {
+    i++
   }
 
-  console.log("private key:", privateKey!)
-  console.log("public key:", publicKey!)
-  console.log(primeKey!)
-  console.log(await toVanityKey(vanity, primeKey!))
-})()
+  // Log guesses per minute
+  const now = Date.now()
+  if (now - lastLog >= 60000) {
+    console.log(`Searches in last minute: ${ i - lastI }`)
+    lastLog = now
+    lastI = i
+  }
+}
+
+console.log("private key:", privateKey!)
+console.log("public key:", publicKey!)
+console.log(primeKey!)
+console.log(await toVanityKey(vanity, primeKey!))
