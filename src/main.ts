1c1279907103f26955d0bd63d353a1052e6a61b8
Mike O'Bank <mike.obank@pm.me>
2025-11-09 18:00:04 +0100

    Arg: cryptoName

--- src/main.ts
+++ src/main.ts
@@ -6,18 +6,18 @@ import {
   publicKeyToPrimaryKey, 
   primaryKeyToFingerprintedName, 
   primaryKeyToFingerprint, 
-  displayFingerprint 
+  displayFingerprint, 
+  toNameKey,
+  isCryptoName
 } from "@vanice/types"
 import createWorkerPool from "./createWorkerPool.ts"
 
-// CLI arg
-if (Deno.args[0] === undefined) {
-  console.error("No vanity name provided")
-  Deno.exit()
-}
-const name = Deno.args[0].trim()
+const hasCryptoArg = Deno.args.length === 2
+const name = hasCryptoArg ? Deno.args[1].trim() : Deno.args[0].trim()
+const cryptoName = hasCryptoArg ? Deno.args[0].trim() : "Schnorr"
+
 if (name === "") {
-  console.error("Empty vanity name provided")
+  console.error("No vanity name provided")
   Deno.exit()
 }
 if (isName(name) === false) {
@@ -25,8 +25,10 @@ if (isName(name) === false) {
   Deno.exit()
 }
 
-// TODO: allow choosing crypto type via CLI arg
-const cryptoName = "Schnorr"
+if (isCryptoName(cryptoName) === false) {
+  console.error(`Unsupported crypto name: ${ cryptoName } (Ed25519, ECDSA, Schnorr are supported)`)
+  Deno.exit()
+}
 
 // Search string
 const primaryName = toPrimaryName(name)
@@ -40,10 +42,11 @@ const { privateKey, publicKey } = await createWorkerPool(
   workerPoolStatus => { console.log(`${ workerPoolStatus.totalAttempts } guesses (${ workerPoolStatus.attemptsPerSecond }/second)`) }
 )
 const primaryKey = publicKeyToPrimaryKey(cryptoName, publicKey)
-console.log("private key:", privateKey)
+console.log(`private key (${ cryptoName }):`, privateKey)
 console.log("private key hex:", displayPrivateKey(cryptoName, privateKey))
-console.log("public key:", publicKey)
+console.log(`public key (${ cryptoName }):`, publicKey)
 console.log("public key hex:", displayPublicKey(cryptoName, publicKey))
 console.log("primary key:", primaryKey)
 console.log("name:", await primaryKeyToFingerprintedName(primaryKey, name))
 console.log("fingerprint:", displayFingerprint(await primaryKeyToFingerprint(primaryKey)))
+console.log("name key:", toNameKey(name, primaryKey))
